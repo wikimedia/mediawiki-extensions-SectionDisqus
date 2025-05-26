aefdfa16871122509c522a0c6e90a904e0e3b13f
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2025-05-26 10:53:15 +0000

    build: Updating dependencies
    composer:
    * mediawiki/mediawiki-codesniffer: 45.0.0 → 47.0.0
    
    npm:
    * eslint-config-wikimedia: 0.28.2 → 0.30.0
    * stylelint-config-wikimedia: 0.17.2 → 0.18.0
    
    Additional changes:
    * eslint: Replaced `wikimedia/client-es5` with `wikimedia/client`.
    * Enable stylelint caching.
    
    Change-Id: I44709a60205114af90a0b21750d346b70fcfb92b

--- SectionDisqus.js
+++ SectionDisqus.js
@@ -7,7 +7,7 @@ window.showDisqusDialog = function ( tooltip ) {
 	} );
 
 	// Use the URL (without parameters) plus the section tooltip as identifier
-	var identifier = location.protocol + '//' + location.hostname + location.pathname + '#!' + tooltip;
+	const identifier = location.protocol + '//' + location.hostname + location.pathname + '#!' + tooltip;
 
 	// Reset Disqus to show the thread corresponding to the clicked section
 	// eslint-disable-next-line no-undef
